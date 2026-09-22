19e83b356cbcc7f56456bb4c28aebb1ef7777153
zow2023 <125123077+zow2023@users.noreply.github.com>
2026-09-22 20:23:00 +0800

    Add transparent gateway settings to settings.js

--- luci-app-meow/htdocs/luci-static/resources/view/meow/settings.js
+++ luci-app-meow/htdocs/luci-static/resources/view/meow/settings.js
@@ -80,6 +80,45 @@ return view.extend({
 		o.default = '9090';
 		o.rmempty = false;
 
+		/* -------------------------------------------------------------- *
+		 * Transparent gateway (forwarded LAN traffic -> tproxy listener)  *
+		 * -------------------------------------------------------------- */
+
+		o = s.option(form.Flag, 'gateway', _('Transparent gateway'),
+			_('Intercept traffic <em>forwarded</em> from LAN clients (transparent ' +
+			  'proxy gateway) via an nftables prerouting REDIRECT to the tproxy ' +
+			  'listener. Requires a <code>listeners:</code> entry with a ' +
+			  'non-loopback <code>listen</code> (e.g. <code>\'::\'</code>) and a ' +
+			  '<code>dns.listen</code> matching the DNS port below in the YAML ' +
+			  'configuration. Rules are loaded after the listener is up and ' +
+			  're-applied on every firewall reload; they are removed when the ' +
+			  'service stops or this option is disabled.'));
+		o.default = o.disabled = false;
+		o.enabled  = '1';
+		o.disabled = '0';
+		o.rmempty = false;
+		o.depends('enabled', '1');
+
+		o = s.option(form.Value, 'tproxy_port', _('tproxy port'),
+			_('Port of the tproxy listener the gateway redirects to. Must match ' +
+			  'the <code>port</code> of the tproxy entry in <code>listeners:</code> ' +
+			  'in the YAML configuration.'));
+		o.datatype = 'port';
+		o.default = '7893';
+		o.placeholder = '7893';
+		o.rmempty = false;
+		o.depends('gateway', '1');
+
+		o = s.option(form.Value, 'dns_port', _('DNS hijack port'),
+			_('LAN DNS queries are DNAT\'d to the gateway\'s meow DNS resolver on ' +
+			  'this port. Must match the <code>dns.listen</code> port in the YAML ' +
+			  'configuration.'));
+		o.datatype = 'port';
+		o.default = '1053';
+		o.placeholder = '1053';
+		o.rmempty = false;
+		o.depends('gateway', '1');
+
 		return m.render();
 	}
 });
